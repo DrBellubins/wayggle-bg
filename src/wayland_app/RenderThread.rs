2509221d3b18a4b30c2fb771f2d24d313b20cfba
DrBellubins <88703839+DrBellubins@users.noreply.github.com>
2026-03-26 19:58:04 -0400

    More render thread work

--- src/wayland_app/RenderThread.rs
+++ src/wayland_app/RenderThread.rs
@@ -1,5 +1,9 @@
 use std::sync::mpsc::{Receiver, Sender};
 
+use crate::wayland_app::graphics::Graphics;
+use crate::wayland_app::AppConfiguration;
+use wayland_client::protocol::{wl_display, wl_surface};
+
 #[derive(Debug)]
 pub enum RenderCommand {
     Render { elapsed: f32 },
@@ -9,12 +13,8 @@ pub enum RenderCommand {
 
 #[derive(Debug)]
 pub enum RenderEvent {
-    FrameComplete { elapsed: f32 },
+    FrameComplete,
 }
-
-use crate::wayland_app::graphics::Graphics;
-use crate::wayland_app::AppConfiguration;
-use wayland_client::protocol::{wl_display, wl_surface};
 
 pub fn run_render_thread(
     display: wl_display::WlDisplay,
@@ -25,18 +25,16 @@ pub fn run_render_thread(
     rx: Receiver<RenderCommand>,
     tx: Sender<RenderEvent>,
 ) {
-    // Create GL/EGL on this thread
     let mut graphics = Graphics::new(&display, &surface, width, height, &conf);
 
-    // Optional: render one initial frame so you see something immediately
-    // (You can also trigger this via a RenderCommand from the Wayland thread.)
-    let _ = tx.send(RenderEvent::FrameComplete { elapsed: 0.0 });
+    // Mark idle initially
+    let _ = tx.send(RenderEvent::FrameComplete);
 
     while let Ok(cmd) = rx.recv() {
         match cmd {
             RenderCommand::Render { elapsed } => {
                 graphics.render(elapsed);
-                let _ = tx.send(RenderEvent::FrameComplete { elapsed });
+                let _ = tx.send(RenderEvent::FrameComplete);
             }
             RenderCommand::Resize { width, height } => {
                 graphics.resize(width, height);
